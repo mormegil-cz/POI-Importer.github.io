28c6172ab98fd6ba86a739f8556ac0ae67152bfc
Mormegil <mormegil@centrum.cz>
2019-08-09 13:24:21 +0200

    Update drobnepamatky.cz na data z 2019-08-04

--- datasets/czechia-drobnepamatky/csv2geojson.cs
+++ datasets/czechia-drobnepamatky/csv2geojson.cs
@@ -1,32 +1,40 @@
 private static readonly int urlPrefixLength = "https://www.drobnepamatky.cz/node/".Length;
 private static readonly Dictionary<string, string> typeClassMapping = new Dictionary<string, string> {
+ { "Altán", "Q961082" },
  { "Boží muka", "Q3395121" },
+ { "Dopravní památka", "Q5003624" },	// ?
+ { "Hodiny", "Q376" },
  { "Hraniční kámen", "Q921099" },
- { "Křížový kámen", "Q38411643" },
- { "Kašna", "Q12029081" },
  { "Kaple", "Q108325" },
  { "Kaplička", "Q14552192" },
+ { "Kašna", "Q12029081" },
+ { "Krajinné umění", "?" },	// ??TODO
+ { "Křížový kámen", "Q38411643" },
  { "Menhir", "Q193475" },
  { "Něco jiného", "?" },
- { "Neznámý", "?" },
+ { "Neznámý", "?" },	//??
  { "Obrázek", "Q478798" },	//?
- { "Pamětní deska", "Q721747" },
- { "Pamětní kříž", "Q2309609" },
+ { "Památná dlažba", "Q3328263" }, //?
  { "Památník", "Q5003624" },
  { "Památný kámen", "Q11734477" },
+ { "Pamětní deska", "Q721747" },
+ { "Pamětní kříž", "Q2309609" },
  { "Plastika", "Q12045520" },	// TODO: Fix WD
  { "Pomník", "Q4989906" },
  { "Pomník padlým", "Q575759" },	// ?
+ { "Reliéf", "Q245117" },
  { "Sloup", "Q4817" },
  { "Smírčí kříž", "Q1640496" },
  { "Socha", "Q179700" },
+ { "Technická památka", "Q1516537" },
+ { "Zvonice", "Q200334" },
  { "Zvonička", "Q10861631" },
 };
 
 void Main()
 {
-	var csvFilename = @"y:\_3rdparty\POI-Importer.github.io\datasets\czechia-drobnepamatky\drobnepamatky-2019-05.csv";
-	using (var geojson = new StreamWriter(@"y:\_3rdparty\POI-Importer.github.io\datasets\czechia-drobnepamatky\drobnepamatky-2019-05.json", false, new UTF8Encoding(false)))
+	var csvFilename = @"y:\_3rdparty\POI-Importer.github.io\datasets\czechia-drobnepamatky\drobnepamatky-2019-08-04.csv";
+	using (var geojson = new StreamWriter(@"y:\_3rdparty\POI-Importer.github.io\datasets\czechia-drobnepamatky\drobnepamatky-2019-08-04.json", false, new UTF8Encoding(false)))
 	{
 		float minLat = Single.PositiveInfinity;
 		float maxLat = Single.NegativeInfinity;
@@ -34,8 +42,8 @@ void Main()
 		float maxLon = Single.NegativeInfinity;
 		foreach (var entry in ReadCsv(csvFilename))
 		{
-			var lat = ParseFloat(entry[1]);
-			var lon = ParseFloat(entry[2]);
+			var lat = ParseFloat(entry[8]);
+			var lon = ParseFloat(entry[7]);
 			minLat = Math.Min(lat, minLat);
 			maxLat = Math.Max(lat, maxLat);
 			minLon = Math.Min(lon, minLon);
@@ -52,15 +60,27 @@ void Main()
 	    ],
 	    ""features"": [");
 		var first = true;
+		/*
+		var set = new HashSet<string>();
 		foreach (var entry in ReadCsv(csvFilename))
 		{
-			var lat = ParseFloat(entry[1]);
-			var lon = ParseFloat(entry[2]);
+			set.Add(entry[6]);
+		}
+		foreach(var k in set.OrderBy(s => s))
+		{
+			Console.WriteLine(k);
+		}
+		return;
+		*/
+		foreach (var entry in ReadCsv(csvFilename))
+		{
+			var lat = ParseFloat(entry[8]);
+			var lon = ParseFloat(entry[7]);
 			var name = entry[0];
-			var typeLabel = entry[5];
+			var typeLabel = entry[6];
 			var typeClass = "http://www.wikidata.org/entity/" + typeClassMapping[typeLabel];
 			var administrative = entry[3];
-			var id = entry[6].Substring(urlPrefixLength);
+			var id = entry[10].Substring(urlPrefixLength);
 
 			if (!first)
 			{
